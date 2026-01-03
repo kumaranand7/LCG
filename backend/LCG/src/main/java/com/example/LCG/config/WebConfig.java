707bf8aad6b2a48186ddfdf44aac76aafd7e68c7
Anand Kumar Gupta <kumarguptaanand1@gmail.com>
2026-01-03 20:10:26 +0530

    space remove

--- backend/LCG/src/main/java/com/example/LCG/config/WebConfig.java
+++ backend/LCG/src/main/java/com/example/LCG/config/WebConfig.java
@@ -1,5 +1,4 @@
 package com.example.LCG.config;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
