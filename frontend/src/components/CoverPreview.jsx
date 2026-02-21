c8390ae292f90959c0dcdc629f30030eebef3e1c
Anand Kumar Gupta <kumarguptaanand1@gmail.com>
2026-02-21 12:39:01 +0530

    merge marker removed

--- frontend/src/components/CoverPreview.jsx
+++ frontend/src/components/CoverPreview.jsx
@@ -1,40 +1,4 @@
 import { TEMPLATES } from "../data/templates";
-<<<<<<< HEAD
-
-export default function CoverPreview({
-  selectedTemplate,
-  generatedImage,
-  loading,
-}) {
-  const template = TEMPLATES.find((t) => t.id === selectedTemplate);
-
-  if (loading) {
-    return <p>Generating cover...</p>;
-  }
-
-  return (
-    <div className="preview">
-      <h3>Preview</h3>
-
-      {/*  If generated image exists → show it */}
-      {generatedImage ? (
-        <>
-          <img src={generatedImage} alt="Generated Cover" />
-          <br /><br />
-          <a href={generatedImage} download>
-            <button>Download</button>
-          </a>
-        </>
-      ) : (
-        /*  Else show selected template preview */
-        <>
-          <img src={template.thumbnail} alt={template.name} />
-          <p style={{ marginTop: "10px", color: "#666" }}>
-            Selected Template: {template.name}
-          </p>
-        </>
-      )}
-=======
 import StyleControls from "./StyleControls";
 
 export default function CoverPreview({
@@ -141,7 +105,6 @@ export default function CoverPreview({
         setStyleConfig={setStyleConfig}
       />
     </div>
->>>>>>> feature/my-new-feature
     </div>
   );
 }
