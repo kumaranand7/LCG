1c09ae0a3a35ebc7d281679308b75605c245f204
Anand Kumar Gupta <kumarguptaanand1@gmail.com>
2026-03-27 21:39:24 +0530

    skills first time add bug fix

--- frontend/src/components/CoverPreview.jsx
+++ frontend/src/components/CoverPreview.jsx
@@ -15,11 +15,6 @@ export default function CoverPreview({
 
   if (loading) return <p>Generating cover...</p>;
 
-  // const overlayStyle = {
-  //   color: styleConfig.textColor,
-  //   textAlign: styleConfig.textAlign,
-  // };
-
 //functioin for file name
  const formatFileName = (text) => {
   return text
